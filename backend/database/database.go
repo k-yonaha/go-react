d9c527f39bc8bf90a1a06023154689849e433418
kyonakyona0615 <kyonakyona0615@gmail.com>
2024-12-26 14:05:38 +0900

    fix postgresに変更

--- backend/database/database.go
+++ backend/database/database.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -13,16 +13,17 @@ var DB *gorm.DB
 
 // データベース接続の初期化
 func Init(user, password, dbname, host, port string) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		user, password, host, port, dbname)
+	log.Println("データベース接続が確立されました:", user)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port)
 	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("データベースへの接続に失敗しました: %v", err)
+		log.Fatalf("データベースの接続に失敗しました: %v", err)
 	}
 
 	// マイグレーション
-	err = DB.AutoMigrate(&models.Room{})
+	err = DB.AutoMigrate(&models.Room{}, &models.RaceSchedule{})
 	if err != nil {
 		log.Fatalf("データベースのマイグレーションに失敗しました。: %v", err)
 	}
